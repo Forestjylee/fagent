3731e0e284cd143f086f9d7962e69410cc9c5ef6
forestjylee <forestjylee@163.com>
2024-12-15 22:46:28 +0800

    test

---
+++ test.py
@@ -0,0 +1,31 @@
+TAVILY_KEY = "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"
+
+import os
+from langchain import hub
+from langchain_community.tools.tavily_search import TavilySearchResults
+from langchain.prompts import PromptTemplate
+from langgraph.prebuilt import create_react_agent
+from langchain_openai import ChatOpenAI
+from langchain_core.output_parsers import JsonOutputParser
+from langchain_community.chat_models import ChatOllama
+
+
+os.environ["TAVILY_API_KEY"] = TAVILY_KEY
+
+llm = ChatOpenAI(
+    model="llama3.2:1b", api_key="ollama", base_url="http://localhost:11434/v1"
+)
+
+tools = [TavilySearchResults(max_results=3)]
+llm_with_tools = llm.bind_tools(tools)
+
+prompt = hub.pull("wfh/react-agent-executor")
+prompt.pretty_print()
+
+agent_executor = create_react_agent(llm, tools, messages_modifier=prompt)
+
+response = agent_executor.invoke(
+    {"messages": [("user", "explain artificial intelligence")]}
+)
+for message in response["messages"]:
+    print(message.content)
